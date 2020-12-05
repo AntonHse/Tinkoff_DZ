ead8fa8f56a79bfdb1371fd9d90ec2131e6e826a
Шуплецов Антон <shupletsov.anton@gmail.com>
2020-12-05 18:38:48 +0300

    Test

--- TinkoffChat/ThemesViewController.swift
+++ TinkoffChat/ThemesViewController.swift
@@ -9,33 +9,30 @@
 import UIKit
 
 class ThemesViewController: UIViewController {
-    
-    
-    var model: Themes?
-    
-    typealias v2CB = (_ infoToReturn :UIColor) ->()
-    var colorToReturn:v2CB?
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-         model = Themes()
-        makeButtonBeautifull(button: theme1ButtonOutlet)
-        makeButtonBeautifull(button: theme2ButtonOutlet)
-        makeButtonBeautifull(button: theme3ButtonOutlet)
+    typealias v2CB = (_ infoToReturn: UIColor) -> Void
 
-    }
-    
-     @IBAction func backButton(_ sender: Any) {
-    dismiss(animated: (true))
-
-    }
-    
-    
     @IBOutlet var theme1ButtonOutlet: UIButton!
     @IBOutlet var theme2ButtonOutlet: UIButton!
     @IBOutlet var theme3ButtonOutlet: UIButton!
     
+    var model: Themes?
+    var colorToReturn:v2CB?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        model = Themes()
+
+        makeButtonBeautifull(button: theme1ButtonOutlet)
+        makeButtonBeautifull(button: theme2ButtonOutlet)
+        makeButtonBeautifull(button: theme3ButtonOutlet)
+    }
+
+    // MARK: - IBAction
+    
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: (true))
+    }
     
     @IBAction func theme1ButtonAction(_ sender: Any) {
         self.view.backgroundColor = model?.theme1
@@ -44,8 +41,6 @@ class ThemesViewController: UIViewController {
         cb((model?.theme1)!)
     }
     
-    
-    
     @IBAction func theme2ButtonAction(_ sender: Any) {
         self.view.backgroundColor = model?.theme2
         
@@ -53,14 +48,13 @@ class ThemesViewController: UIViewController {
         cb((model?.theme2)!)
         
     }
+
     @IBAction func theme3ButtonAction(_ sender: Any) {
         self.view.backgroundColor = model?.theme3
         guard let cb = colorToReturn else {return}
         cb((model?.theme3)!)
         
     }
-    
-
 }
 
 func makeButtonBeautifull(button: UIButton){
